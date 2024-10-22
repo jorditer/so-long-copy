dfe080c5d9dd073c653b66e22fd91bb93d3f7bbb
Javier Andreu <147188123+dracudev@users.noreply.github.com>
2024-10-22 17:35:36 +0200

    Update delete.c

--- src/delete.c
+++ src/delete.c
@@ -14,40 +14,46 @@
 
 void	delete_textures(t_game *game)
 {
-	if (game->txt->floor[0])
-		mlx_delete_texture(game->txt->floor[0]);
-	if (game->txt->floor[1])
-		mlx_delete_texture(game->txt->floor[1]);
-	if (game->txt->wall)
-		mlx_delete_texture(game->txt->wall);
-	if (game->txt->exit[0])
-		mlx_delete_texture(game->txt->exit[0]);
-	if (game->txt->exit[1])
-		mlx_delete_texture(game->txt->exit[1]);
-	if (game->txt->pnj)
-		mlx_delete_texture(game->txt->pnj);
-	if (game->txt->collect)
-		mlx_delete_texture(game->txt->collect);
 	if (game->txt)
+    	{
+		if (game->txt->floor[0])
+			mlx_delete_texture(game->txt->floor[0]);
+		if (game->txt->floor[1])
+			mlx_delete_texture(game->txt->floor[1]);
+		if (game->txt->wall)
+			mlx_delete_texture(game->txt->wall);
+		if (game->txt->exit[0])
+			mlx_delete_texture(game->txt->exit[0]);
+		if (game->txt->exit[1])
+			mlx_delete_texture(game->txt->exit[1]);
+		if (game->txt->pnj)
+			mlx_delete_texture(game->txt->pnj);
+		if (game->txt->collect)
+			mlx_delete_texture(game->txt->collect);
 		free(game->txt);
+		game->txt = NULL;
+	}
 }
 
 void	delete_images(t_game *game)
 {
-	if (game->img->floor[0])
-		mlx_delete_image(game->mlx, game->img->floor[0]);
-	if (game->img->floor[1])
-		mlx_delete_image(game->mlx, game->img->floor[1]);
-	if (game->img->wall)
-		mlx_delete_image(game->mlx, game->img->wall);
-	if (game->img->exit[0])
-		mlx_delete_image(game->mlx, game->img->exit[0]);
-	if (game->img->exit[1])
-		mlx_delete_image(game->mlx, game->img->exit[1]);
-	if (game->img->pnj)
-		mlx_delete_image(game->mlx, game->img->pnj);
-	if (game->img->collect)
-		mlx_delete_image(game->mlx, game->img->collect);
-	if (game->img);
+	if (game->img)
+	{
+		if (game->img->floor[0])
+			mlx_delete_image(game->mlx, game->img->floor[0]);
+		if (game->img->floor[1])
+			mlx_delete_image(game->mlx, game->img->floor[1]);
+		if (game->img->wall)
+			mlx_delete_image(game->mlx, game->img->wall);
+		if (game->img->exit[0])
+			mlx_delete_image(game->mlx, game->img->exit[0]);
+		if (game->img->exit[1])
+			mlx_delete_image(game->mlx, game->img->exit[1]);
+		if (game->img->pnj)
+			mlx_delete_image(game->mlx, game->img->pnj);
+		if (game->img->collect)
+			mlx_delete_image(game->mlx, game->img->collect);
 		free(game->img);
+		game->img = NULL;
+	}
 }
